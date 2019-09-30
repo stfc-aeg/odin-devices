8b15b628e1344633641e8bd3859734f581c0dc2e
Adam Neaves <adam.neaves@stfc.ac.uk>
2019-09-30 13:25:19 +0100

    Fixed issue with init of si570 class in fixture

--- tests/test_si570.py
+++ tests/test_si570.py
@@ -7,13 +7,17 @@ import sys
 import pytest
 
 if sys.version_info[0] == 3:  # pragma: no cover
-    from unittest.mock import Mock, call
+    from unittest.mock import Mock, call, patch
 else:                         # pragma: no cover
-    from mock import Mock, call
+    from mock import Mock, call, patch
 
-sys.modules['smbus'] = Mock()
+# sys.modules['smbus'] = Mock()
 from odin_devices.si570 import SI570
 from odin_devices.i2c_device import I2CDevice, I2CException
+
+
+def readList_side_effect(reg, length):
+    return [i for i in range(length)]
 
 
 class si570TestFixture(object):
@@ -22,7 +26,11 @@ class si570TestFixture(object):
         self.address = 0x5d
         self.busnum = 1
         self.model = SI570.SI570_C
-        self.driver = SI570(self.address, self.model, busnum=self.busnum)
+        with patch("odin_devices.i2c_device.I2CDevice.readU8") as mocked_readu8, \
+             patch("odin_devices.i2c_device.I2CDevice.readList") as mocked_readlist:
+            mocked_readu8.return_value = 0
+            mocked_readlist.side_effect = readList_side_effect
+            self.driver = SI570(self.address, self.model, busnum=self.busnum)
 
 
 @pytest.fixture(scope="class")
@@ -33,7 +41,7 @@ def test_si570_driver():
 
 class TestSI570():
 
-    @pytest.mark.skip(reason="Getting an out of index error")
+    # @pytest.mark.skip(reason="Getting an out of index error")
     def test_fake(self, test_si570_driver):
         # dummy test case as placeholder
         assert True
