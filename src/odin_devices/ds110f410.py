ed1815eddc9eebfbfda10b33cb5766c61bb9c530
Joseph Nobes <joseph.nobes@stfc.ac.uk>
2020-11-17 13:32:20 +0000

    Created channel inner class for device channels 0-3 and all
    Originally, channel number was going to be passed to register access functions
    and be ignored (or a default value assumed) if it was not needed.
    
    This has now been updated to have a separate class for channel-related functions,
    with the ds110 instance having 5 instances as attributes (4 channels including an
    all channel for writing to all channels). This will make the divide between control
    of device-specific and channel-specific settings more clear.
    
    Some functionality has been added to test this structure out, including reset
    functions for both the channel registers and shared control registers.

--- src/odin_devices/ds110f410.py
+++ src/odin_devices/ds110f410.py
@@ -20,12 +20,17 @@ class _Field:
 
     The DS110 has register IDs that overlap for 'control/shared' and 'channel' registers. Given
     that this is a field property, it is appears in the class as register_group.
+
+    Some registers are write_only (e.g. reset triggers), so the normal process of reading current
+    register values to replace some and write back will not work. Therefore these fields are marked
+    with 'write_only=True' to ensure the field writing function does not read previous reg state.
     """
-    def __init__(self, register, register_group, startbit, length):
+    def __init__(self, register, register_group, startbit, length, write_only=False):
         self.register = register
         self.register_group = register_group
         self.startbit = startbit
         self.length = length
+        self.write_only = write_only
 
     def get_endbit(self):
         return (self.startbit - (self.length - 1))
@@ -34,15 +39,14 @@ class _Field:
 class DS110F410(I2CDevice):
 
     # Channel/shared register group selection
-    _REG_GROUP_Shared = 1
-    _REG_GROUP_Channel = 0
-    CHANNEL_0 = 0
-    CHANNEL_1 = 1
-    CHANNEL_2 = 2
-    CHANNEL_3 = 3
-    CHANNEL_All = 4     # Applies to write only
-    _REG_GROUP_SELECTED = _REG_GROUP_Channel        # By default shared register write is off
-    _REG_CHANNEL_SELECTED = CHANNEL_0               # By default channel 0 is selected
+    _REG_GRP_Shared = 1
+    _REG_GRP_Channel = 0
+
+    _CHANNEL_0      = 0
+    _CHANNEL_1      = 1
+    _CHANNEL_2      = 2
+    _CHANNEL_3      = 3
+    _CHANNEL_All    = 4         # Applies to write only
 
     # System reset signals
     RST_CORE    = 0b1000
@@ -51,8 +55,19 @@ class DS110F410(I2CDevice):
     RST_VCO     = 0b0001
     RST_ALL     = 0b1111
 
-    # Register Fields
-    _FIELD_Channel_Reset = _Field(0x00, _REG_GROUP_Channel, 3, 4)   # Combined channel reset bits
+    # Register Fields (Channel)
+    _FIELD_Chn_Reset = _Field(0x00, _REG_GRP_Channel, 3, 4)         # Combined channel reset bits
+
+    _FIELD_Chn_CDR_LL_INT = _Field(0x01, _REG_GRP_Channel, 4, 1)    # CDR Lock Loss Interrupt
+    _FIELD_Chn_SIG_DET_LOSS_INT = _Field(0x01, _REG_GRP_Channel, 0, 1)  # SD Loss Interrupt
+
+    _FIELD_Chn_Status = _Field(0x02, _REG_GRP_Channel, 7, 8)        # Channel status full byte
+
+    _FIELD_Chn_EOM_VR_Lim_Err = _Field(0x30, _REG_GRP_Channel, 5, 1)
+    _FIELD_Chn_HEO_VEO_INT = _Field(0x30, _REG_GRP_Channel, 4, 1)       # Cleared on read
+
+    # Register Fields (Shared / Control)
+    _FIELD_Shr_Reset = _Field(0x04, _REG_GRP_Shared, 6, 1)
 
     def __init__(self, address=0x18, **kwargs):
         """
@@ -64,7 +79,17 @@ class DS110F410(I2CDevice):
         I2CDevice.__init__(self, address, **kwargs)
         logger.info("Created new ds110f410 instance with address 0x{:02X}.".format(address))
 
-        self.reset(CHANNEL_All, RST_ALL)        # Get all channels in known reset state
+        # Set up channels
+        self.channel_0 = _Channel(self, self._CHANNEL_0)
+        self.channel_1 = _Channel(self, self._CHANNEL_1)
+        self.channel_2 = _Channel(self, self._CHANNEL_2)
+        self.channel_3 = _Channel(self, self._CHANNEL_3)
+        self.channel_all = _Channel(self, self._CHANNEL_All)
+        self.channel_all.reset(RST_ALL)         # Get all channels in known reset state
+
+        # Set up register access current state
+        self._REG_GRP_SELECTED = _REG_GRP_Channel       # By default shared register write is off
+        self._REG_CHANNEL_SELECTED = _CHANNEL_0         # By default channel 0 is selected
 
     """
     Utility Functions:
@@ -88,45 +113,48 @@ class DS110F410(I2CDevice):
         well as setting the WRITE_ALL_CH bit if all channels are being written. The EN_CH_SMB bit
         is always set to ensure that channel access is enabled.
 
-        :param register_group: Register group to use. _REG_GROUP_Shared to set a shared control
-                                register, or _REG_GROUP_Channel to target only a channel (or all
+        :param register_group: Register group to use. _REG_GRP_Shared to set a shared control
+                                register, or _REG_GRP_Channel to target only a channel (or all
                                 channels), specified below.
-        :param register_channel:    Channel written to if _REG_GROUP_Channel is selected.
+        :param register_channel:    Channel written to if _REG_GRP_Channel is selected.
         """
-        if register_group == _REG_GROUP_Shared:
+        if register_group == _REG_GRP_Shared:
             self.write8(0xFF, 0b0000)       # WRITE_ALL_CH = 0, EN_CH_SMB = 0, CH_SEL = 0
-        elif register_group == _REG_GROUP_Channel:
-            if register_channel in [CHANNEL_0, CHANNEL_1, CHANNEL_2, CHANNEL_3]:
+        elif register_group == _REG_GRP_Channel:
+            if register_channel in [self._CHANNEL_0,
+                                    self._CHANNEL_1,
+                                    self._CHANNEL_2,
+                                    self._CHANNEL_3]:
                 self.write8(0xFF,
                             0b0100 |                # WRITE_ALL_CH = 0, EN_CH_SMB = 1
                             (register_channel))
-            elif register_channel == CHANNEL_All:
+            elif register_channel == self._CHANNEL_All:
                 self.write8(0xFF,
                             0b1100 |                # WRITE_ALL_CH = 1, EN_CH_SMB = 1
                             (register_channel))
             else:
                 raise I2CException("Invalid register channel."
-                                   " Select CHANNEL_0-CHANNEL3 or CHANNEL_All.")
+                                   " Select self._CHANNEL_0-CHANNEL3 or self._CHANNEL_All.")
         else:
             raise I2CExcecption("Invalid register group."
-                                " Select _REG_GROUP_Channel or _REG_GROUP_Shared.")
+                                " Select _REG_GRP_Channel or _REG_GRP_Shared.")
 
-    def _write_register(register_group, register_channel=CHANNEL_All, register, value):
+    def _write_register(register_group, register_channel=self._CHANNEL_All, register, value):
         """
         Wrapper for the I2CDevice write8 function.
         Ensures that the correct register group is specified for writes, and avoids unnecessary
         transmissions setting the group each time if it is already correct.
 
-        If the register_group is set as _REG_GROUP_Shared, the channel is irrelevant.
+        If the register_group is set as _REG_GRP_Shared, the channel is irrelevant.
 
-        :param register_group: Register group to use. _REG_GROUP_Shared to set a shared control
-                                register, or _REG_GROUP_Channel to target only a channel (or all
+        :param register_group: Register group to use. _REG_GRP_Shared to set a shared control
+                                register, or _REG_GRP_Channel to target only a channel (or all
                                 channels), specified below.
-        :param register_channel:    Channel written to if _REG_GROUP_Channel is selected.
+        :param register_channel:    Channel written to if _REG_GRP_Channel is selected.
         :param register: Register ID to be written
         :param value: Value to be written
         """
-        if ((register_group != self._REG_GROUP_SELECTED) or
+        if ((register_group != self._REG_GRP_SELECTED) or
             (register_channel != self._REG_CHANNEL_SELECTED)):
             _reg_ch_sel(register_group, register_channel)
         self.write8(register, value)
@@ -137,19 +165,19 @@ class DS110F410(I2CDevice):
         Ensures that the correct register group is specified for reads, and avoids unnecessary
         transmissions setting the group each time if it is already correct.
 
-        :param register_group: Register group to use. _REG_GROUP_Shared to set a shared control
-                                register, or _REG_GROUP_Channel to target only a channel (or all
+        :param register_group: Register group to use. _REG_GRP_Shared to set a shared control
+                                register, or _REG_GRP_Channel to target only a channel (or all
                                 channels), specified below.
-        :param register_channel:    Channel written to if _REG_GROUP_Channel is selected.
+        :param register_channel:    Channel written to if _REG_GRP_Channel is selected.
         :param register: Register ID to be read
         :return: Value read from the register
         """
-        if ((register_group != self._REG_GROUP_SELECTED) or
+        if ((register_group != self._REG_GRP_SELECTED) or
             (register_channel != self._REG_CHANNEL_SELECTED)):
             _reg_ch_sel(register_group, register_channel)
         return self.readU8(register)
 
-    def _write_field(field, channel=CHANNEL_All, value, verify=False):
+    def _write_field(field, channel=self._CHANNEL_All, value, verify=False):
         """
         Write a field of <=8 bits into an 8-bit register.
         Field bits are masked to preserve other settings held within the same register.
@@ -167,11 +195,11 @@ class DS110F410(I2CDevice):
             value,field.startbit,field.get_endbit(),field.register)) #TODO update logging
 
         # warn about channel selection
-        if field.register_group == _REG_GROUP_Shared:
-            if channel != CHANNEL_All:
+        if field.register_group == _REG_GRP_Shared:
+            if channel != self._CHANNEL_All:
                 logger.warning(
                         "Field is shared, but channel specified. Channel will be ignored.")
-        elif field.register_group == _REG_GROUP_Channel:
+        elif field.register_group == _REG_GRP_Channel:
             if channel == CHANNEL_ALL:
                 logger.warning(
                         "Channel set to -or defaulting to- ALL channels.")
@@ -182,7 +210,10 @@ class DS110F410(I2CDevice):
                     "Value {} does not fit in specified field of length {}.".format(
                         value, field.length))
 
-        old_value = self._read_register(field.register_group, channel, field.register)
+        if (field.write_only):
+            old_value = 0x00
+        else:
+            old_value = self._read_register(field.register_group, channel, field.register)
         new_msk = (0xff >> (8-field.length)) << field.get_endbit()
         logger.debug("Register {}: field start: {}, field end: {} -> mask {:b}".format(
             field.register,field.startbit,field.get_endbit(), new_msk))
@@ -200,7 +231,7 @@ class DS110F410(I2CDevice):
                         "Value {} was not successfully written to Field {}".format(
                             value, field))
 
-    def _read_field(field, channel=CHANNEL_All):
+    def _read_field(field, channel=self._CHANNEL_All):
         """
         Read only the field-specific bits from the relevant register
 
@@ -212,14 +243,14 @@ class DS110F410(I2CDevice):
             field.startbit,field.length,field.register)) #TODO update to include channels
 
         # Generate warnings / exception for incorrect reg group/channel combination
-        if ((field.register_group == _REG_GROUP_Shared) and
-            (channel in [CHANNEL_0, CHANNEL_1, CHANNEL_2, CHANNEL_3])):
+        if ((field.register_group == _REG_GRP_Shared) and
+            (channel in [self._CHANNEL_0, self._CHANNEL_1, self._CHANNEL_2, self._CHANNEL_3])):
             logger.warning(
                     "Field is a shared / control register but channel was specified, ignoring...")
-        elif ((field.register_group == _REG_GROUP_Channel) and
-              (channel == CHANNEL_All)):
+        elif ((field.register_group == _REG_GRP_Channel) and
+              (channel == self._CHANNEL_All)):
             raise I2CException(
-                    "Field read requires a specific channel, plesase specify CHANNEL_0-3.")
+                    "Field read requires a specific channel, plesase specify self._CHANNEL_0-3.")
 
         raw_register_value = self._read_register(field.register_group, channel, field.register)
         logger.debug("Raw value: {0:b}".format(raw_register_value))
@@ -236,31 +267,75 @@ class DS110F410(I2CDevice):
     """
     Device Action Functions:
     """
-    def reset(channel, selection=[RST_ALL]):
-        """
-        Resets entire device or only sections of it for a given channel.
-        Either specify ALL, or supply a list of other chosen RESET_X devices.
-        Reset bits are self-clearing.
+    def reset(reset_channel_registers=True):
+        if (reset_channel_registers):
+            self.channel_all.reset(RST_ALL)                 # Full Reset all channels
+        _write_field(DS110F410._FIELD_Shr_Reset, 0b1)    # Reset shared / control registers
 
-        :param channel:     Channel to reset. Specify CHANNEL_0-3 or CHANNEL_All.
-        :param selection:   A list containing systems to reset. No argument or a list containing
-                            RST_ALL will reset all systems. Others: RST_CORE, RST_REGS, RST_REFCLK,
-                            RST_VCO
-        """
-        if type(selection) != list:
-            raise I2CException(
-                    "Argument supplied was not a list")
-        if RST_ALL in selection:
-            # There is no point in checking other values
-            _write_field(_FIELD_Channel_Reset, channel, RST_ALL)
-        else:
-            # OR values to combine into single reset write
-            rst_combined = 0
-            for rst_bit in selection:
-                if rst_bit in [RST_CORE, RST_REGS, RST_REFCLK, RST_VCO]:
-                    rst_combined |= rst_bit
-                else:
-                    raise I2CException(
-                            "Incorrect reset sigal supplied. List items may only be:"
-                            " RST_CORE, RST_REGS, RST_REFCLK, RST_VCO")
-            _write_field(_FIELD_Channel_Reset, channel, rst_combined)
+
+    class _Channel:
+
+        def __init__(self, ds110_instance, channel_id):
+            self.CID = channel_id
+            self._ds110 = ds110_instance
+
+        def get_status(self):
+            # Taken from Thomas' version and adapted
+            print ' Reset Reg: ' + hex(self._read_field(DS110F410._FIELD_Chn_Reset, self.CID))
+
+            tmp = self.ds110._read_field(DS110F410._FIELD_Chn_CDR_LL_INT, self.CID)
+            print 'CDR LOCK LOSS INT: ' + str(tmp)
+            tmp = self.ds110._read_field(DS110F410._FIELD_Chn_SIG_DET_LOSS_INT, self.CID)
+            print 'CDR LOCK LOSS INT: ' + str(tmp)
+
+            tmp = self.ds110._read_field(_FIELD_Chn_EOM_VR_Lim_Err, self.CID)
+            print 'EOM VRANGE LIMIT ERROR: ' + str(tmp)
+            tmp = self.ds110._read_field(_FIELD_Chn_HEO_VEO_INT, self.CID)
+            print 'HEO VEO INT: ' + str(tmp)
+
+            tmp = self.ds110._read_field(_FIELD_Chn_Status, self.CID)
+            print hex(tmp)
+            if (tmp & 0b10000000): print 'Comp LPF Low'
+            if (tmp & 0b01000000): print 'Comp LPF High'
+            if (tmp & 0b00100000): print 'Single Bit Limit Reached'
+            if (tmp & 0b00010000): print 'CDR Lock'
+            if (tmp & 0b00001000): print 'LOCK'
+            if (tmp & 0b00000100): print 'Fail Lock Check'
+            if (tmp & 0b00000010): print 'Auto Adapt Complete'
+            if (tmp & 0b00000011): print 'PPM Count Met'
+
+            heo = self.ds110._read_field(_FIELD_Chn_HEO_Val, self.CID)
+            veo = self.ds110._read_field(_FIELD_Chn_VEO_Val, seld.CID)
+            print 'HEO ' + str(heo)
+            print 'VEO ' + str(veo)
+
+        def reset(self, selection=[RST_ALL]):
+            """
+            Resets entire device or only sections of it for a given channel.
+            Either specify ALL, or supply a list of other chosen RESET_X devices.
+            Reset bits are self-clearing.
+
+            :param channel:     Channel to reset. Specify self._CHANNEL_0-3 or self._CHANNEL_All.
+            :param selection:   A list containing systems to reset. No argument or a list containing
+                                RST_ALL will reset all systems. Others: RST_CORE, RST_REGS, RST_REFCLK,
+                                RST_VCO
+            """
+            if type(selection) != list:
+                raise I2CException(
+                        "Argument supplied was not a list")
+            if RST_ALL in selection:
+                # There is no point in checking other values
+                self._ds110._write_field(ds110f410._field_channel_reset, self.cid, rst_all)
+            else:
+                # or values to combine into single reset write
+                rst_combined = 0
+                for rst_bit in selection:
+                    if rst_bit in [rst_core, rst_regs, rst_refclk, rst_vco]:
+                        rst_combined |= rst_bit
+                    else:
+                        raise i2cexception(
+                                "incorrect reset sigal supplied. list items may only be:"
+                                " rst_core, rst_regs, rst_refclk, rst_vco")
+                self._ds110._write_field(ds110f410._field_channel_reset, self.cid, rst_combined)
+
+
