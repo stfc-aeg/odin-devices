f7910a6bec5600569513a376bc4744b42f976a13
Joseph Nobes <joseph.nobes@stfc.ac.uk>
2021-01-08 16:54:21 +0000

    Support for asynchronous events added, untested on HW
    Asynchronous events have now been added using concurrent.futures to create tasks in a
    ThreadPool for each monitor. These monitors are self-terminating when requested through
    the remove_pin_event_callback() function. The user is able to supply their own callback.
    
    Pins are also now checked for availability before requests are tried using the is_used()
    function. This requires the pin information to be in sync with the kernel, which requires
    the Line.update() function to be available (which it is not in earlier versions of gpiod).
    This is checked at import. The version of libgpiod has been updated in the CFI DAQ
    PetaLinux control project to meet this requirement.

--- src/odin_devices/gpio_bus.py
+++ src/odin_devices/gpio_bus.py
@@ -1,6 +1,40 @@
-import gpiod
+import logging
+logger = logging.getLogger('odin_devices.gpio_bus')
 
-# TODO can I use with to protect the resources being lost? Pins MUST be released
+class GPIOException(Exception):
+    pass
+
+"""
+Check dependencies and environment:
+"""
+
+try:
+    import gpiod
+except ModuleNotFoundException:
+    raise GPIOException(
+            "gpiod module not found. "
+            "This module requires libgpiod to be compiled with python bindings enabled. "
+            "See https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git/about/")
+
+# If concurrence is not available, module should still be able to be used without events.
+_ASYNC_AVAIL = True
+try:
+    import concurrent.futures
+except ModuleNotFoundException:
+    _ASYNC_AVAIL = False
+    logger.warning(
+            "concurrent.futures module not available, asynchronous events not supported")
+
+# This module requires a new enough version of libgpiod that the line.update() function exists
+try:
+    gpiod.Line.update
+except AttributeError:
+    raise GPIOException(
+            "This module requries a version of libgpiod that includes gpiod.Line.update()")
+
+"""
+GPIO Bus Class:
+"""
 
 class GPIO_Bus():
     DIR_INPUT = gpiod.LINE_REQ_DIR_IN
@@ -8,6 +42,13 @@ class GPIO_Bus():
 
     ACTIVE_L = gpiod.Line.ACTIVE_LOW
     ACTIVE_H = gpiod.Line.ACTIVE_HIGH
+
+    EVENT_RISING = gpiod.LineEvent.RISING_EDGE
+    EVENT_FALLING = gpiod.LineEvent.FALLING_EDGE
+
+    EV_REQ_RISING = gpiod.LINE_REQ_EV_RISING_EDGE
+    EV_REQ_FALLING = gpiod.LINE_REQ_EV_FALLING_EDGE
+    EV_REQ_BOTH_EDGES = gpiod.LINE_REQ_EV_BOTH_EDGES
 
     def __init__(self, width, chipno=0, system_offset=0):
         # Select the specified GPIO chip
@@ -21,6 +62,14 @@ class GPIO_Bus():
         # Set the consumer name
         self._consumer_name = "odin_gpio_bus"
 
+        # Set up for concurrency if available
+        self._executor = None       # Will manage futures for events being awaited
+        self._monitors = {}          # Will hold references to futures and related lines
+        self._monitor_remove_pending = []   # List of pin indexes that should stop monitoring
+
+    """
+    Bus and Consumer Management
+    """
     def get_consumer_name(self):
         return self._consumer_name
 
@@ -40,13 +89,32 @@ class GPIO_Bus():
 
         self._gpio_chip.get_lines(range(self._system_offset, self._system_offset + new_width))
 
+    """
+    Pin Requests
+    """
+    def _check_pin_avail(self, index, ignore_current=False):
+        self._master_linebulk.to_list()[index].update()
+        # Check if the pin is in range
+        if index >= self._width:
+            raise GPIOException ("GPIO Pin index out of range, bus width: {}".format(self._width))
+
+        if not ignore_current:
+            # Check pin is not already requested for other operations by this user
+            if self._master_linebulk.to_list()[index].is_requested():
+                raise GPIOException ("User has already requested ownership of this line")
+
+        # Check pin is not already in use by the kernel or another user
+        if self._master_linebulk.to_list()[index].is_used():
+            raise GPIOException ("This line is already in use by another user or the kernel")
+
     def get_pin(self, index, direction, active_l=False, no_request=False):
-        # TODO Check pin is within range, and unrequested
-        # TODO if requested, but by current application, call with no_request and warn
+
+        # check pin is valid and available
+        self._check_pin_avail(index)
 
         # Create GPIO_Pin wrapper around master linebulk pin
         line = self._master_linebulk.to_list()[index]
-        if no_request == False:
+        if no_request == False and line.is_requested() == False:
             if active_l:
                 flags = LINE_REQ_FLAG_ACTIVE_LOW
             else:
@@ -55,12 +123,18 @@ class GPIO_Bus():
                          type = direction,
                          flags = flags,
                          default_val = 0)
+        elif line.is_requested():
+            # Warn user if they have already requested this line, but still return it
+            logger.warning("This line is already requested, returning handle")
 
         return line
 
     def get_bulk_pins(self, indexes, direction, active_l=False, no_request=False):
-        # TODO check pin indexes are all in range, and unrequested
         # TODO if requested, but by current consumer, call with no_request and warn
+
+        # Check pins are valid and available
+        for index in indexes:
+            self._check_pin_avail(index, ignore_current=True)
 
         # Create GPIO_Bulk_Pins wrapper around master linebulk pin
         lines = self._master_linebulk.get_lines(indexes)
@@ -79,8 +153,59 @@ class GPIO_Bus():
     def release_all_consumed(self):
         self._master_linebulk.release()
 
+    """
+    Asynchronous Event Callbacks:
+    """
+    def _event_monitor_task(self, callback_function, index, line):
+        # ThreadPool futures can only be terminated individually by finishing work
+        while not index in self._monitor_remove_pending:
+            # Check non-blocking so that event monitor removal is polled
+            if line.event_wait(sec=1):
+                event = line.event_read()
+                callback_function(event.type, index)    # Call the user's callback
 
-# Example buses
+    def register_pin_event_callback(self, index, event_request_type, callback_function):
+        # Callback has 2 arguments, event and index. Event is one of those defined at the top
+        # Event request is one of those defined at the top
+
+        # check pin is valid and available
+        self._check_pin_avail(index)
+
+        # Check module has been imported to allow asynchronous code
+        if not _ASYNC_AVAIL:
+            raise GPIOException ("Asynchronous operations not available, no concurrent.futures module")
+
+        # Create event request
+        line = self._master_linebulk.to_list()[index]
+        line.request(consumer=self._consumer_name,
+                     type=event_request_type)
+
+        # Register callback
+        if self._executor == None:
+            self._executor = concurrent.futures.ThreadPoolExecutor(max_workers=None)
+        self._monitors[index] = (self._executor.submit(self._event_monitor_task, callback_function, index, line))
+
+    def remove_pin_event_callback(self, index):
+        # Remove callback async function
+        self._monitor_remove_pending.append(index)
+
+        while not self._monitors[index].done():
+            pass
+
+        self._monitor_remove_pending.remove(index)
+        self._monitors.pop(index)
+
+        # Free pin
+        line = self.get_pin(index, None, False, True)
+        line.release()
+
+    def report_registered_events(self):
+        #TODO list all pins in the monitor list
+        pass
+
+"""
+Example Buses:
+"""
 GPIO_Zynq = GPIO_Bus(12, 0, 54)         # Width will need to match EMIO GPIO width
 GPIO_ZynqMP = GPIO_Bus(12, 0, 78)       # Width will need to match EMIO GPIO width
 
